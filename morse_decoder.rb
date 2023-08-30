6395a6996b2ca6125c6ff79e53f1fbf126df5ce7
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-08-30 13:08:55 -0600

    Fix offenses

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,38 +1,32 @@
 # frozen_string_literal: true
 
-# Creating a Morse Code Dictionary
 MORSE_CODE = {
-  'A' => '.-',     'B' => '-...',   'C' => '-.-.',
-  'D' => '-..',    'E' => '.',      'F' => '..-.',
-  'G' => '--.',    'H' => '....',   'I' => '..',
-  'J' => '.---',   'K' => '-.-',    'L' => '.-..',
-  'M' => '--',     'N' => '-.',     'O' => '---',
-  'P' => '.--.',   'Q' => '--.-',   'R' => '.-.',
-  'S' => '...',    'T' => '-',      'U' => '..-',
-  'V' => '...-',   'W' => '.--',    'X' => '-..-',
-  'Y' => '-.--',   'Z' => '--..',
-
-  '1' => '.----',  '2' => '..---',  '3' => '...--',
-  '4' => '....-',  '5' => '.....',  '6' => '-....',
-  '7' => '--...',  '8' => '---..',  '9' => '----.',
+  'A' => '.-', 'B' => '-...', 'C' => '-.-.',
+  'D' => '-..', 'E' => '.', 'F' => '..-.',
+  'G' => '--.', 'H' => '....', 'I' => '..',
+  'J' => '.---', 'K' => '-.-', 'L' => '.-..',
+  'M' => '--', 'N' => '-.', 'O' => '---',
+  'P' => '.--.', 'Q' => '--.-', 'R' => '.-.',
+  'S' => '...', 'T' => '-', 'U' => '..-',
+  'V' => '...-', 'W' => '.--', 'X' => '-..-',
+  'Y' => '-.--', 'Z' => '--..',
+  '1' => '.----', '2' => '..---', '3' => '...--',
+  '4' => '....-', '5' => '.....', '6' => '-....',
+  '7' => '--...', '8' => '---..', '9' => '----.',
   '0' => '-----'
 }.freeze
 
-# Method to decode a Morse code character
 def decode_char(morse_char)
   MORSE_CODE.key(morse_char)
 end
 
-# Method to decode an entire word in Morse code
 def decode_word(morse_word)
-  morse_word.split(' ').map { |char| decode_char(char) }.join
+  morse_word.split.map { |char| decode_char(char) }.join
 end
 
-# Method to decode the entire message in Morse code
 def decode(message)
   message.split('   ').map { |word| decode_word(word) }.join(' ')
 end
 
-# Using IRB to decode the message:
 message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 puts decode(message)
